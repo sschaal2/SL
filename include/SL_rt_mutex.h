5bd25659d82db6f34f1e59b6358879b22ba60922
Mrinal Kalakrishnan <kalakris@usc.edu>
2010-11-10 19:33:10 -0800

    Added wrappers for realtime condition variables

--- include/SL_rt_mutex.h
+++ include/SL_rt_mutex.h
@@ -17,6 +17,7 @@
 
 #ifdef __XENO__
 #include <native/mutex.h>
+#include <native/cond.h>
 #else
 #include <pthread.h>
 #endif
@@ -27,8 +28,10 @@ extern "C" {
 
 #ifdef __XENO__
   typedef RT_MUTEX sl_rt_mutex;
+  typedef RT_COND sl_rt_cond;
 #else
   typedef pthread_mutex_t sl_rt_mutex;
+  typedef pthread_cond_t sl_rt_cond;
 #endif
 
 int sl_rt_mutex_init(sl_rt_mutex* mutex);
@@ -36,6 +39,11 @@ int sl_rt_mutex_destroy(sl_rt_mutex* mutex);
 int sl_rt_mutex_lock(sl_rt_mutex* mutex);
 int sl_rt_mutex_trylock(sl_rt_mutex* mutex);
 int sl_rt_mutex_unlock(sl_rt_mutex* mutex);
+
+int sl_rt_cond_init(sl_rt_cond* cond);
+int sl_rt_cond_destroy(sl_rt_cond* cond);
+int sl_rt_cond_signal(sl_rt_cond* cond);
+int sl_rt_cond_wait(sl_rt_cond* cond, sl_rt_mutex* mutex);
 
 #ifdef __cplusplus
 }
@@ -86,6 +94,42 @@ inline int sl_rt_mutex_unlock(sl_rt_mutex* mutex)
   return rt_mutex_release(mutex);
 #else
   return pthread_mutex_unlock(mutex);
+#endif
+}
+
+inline int sl_rt_cond_init(sl_rt_cond* cond)
+{
+#ifdef __XENO__
+  return rt_cond_create(cond, NULL);
+#else
+  return pthread_cond_init(cond, NULL);
+#endif
+}
+
+inline int sl_rt_cond_destroy(sl_rt_cond* cond)
+{
+#ifdef __XENO__
+  return rt_cond_delete(cond);
+#else
+  return pthread_cond_destroy(cond);
+#endif
+}
+
+inline int sl_rt_cond_signal(sl_rt_cond* cond)
+{
+#ifdef __XENO__
+  return rt_cond_signal(cond);
+#else
+  return pthread_cond_signal(cond);
+#endif
+}
+
+inline int sl_rt_cond_wait(sl_rt_cond* cond, sl_rt_mutex* mutex)
+{
+#ifdef __XENO__
+  return rt_cond_wait(cond, mutex, TM_INFINITE);
+#else
+  return pthread_cond_wait(cond, mutex);
 #endif
 }
 
