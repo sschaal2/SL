f9db7e4d3a955cb4f46af3450bf3f7903928cae5
CLMC-Lab User <sschaal@usc.edu>
2011-01-28 14:00:29 -0800

    updates for interface force/torque computations

--- include/SL_common.h
+++ include/SL_common.h
@@ -88,6 +88,11 @@ extern "C" {
   void prismaticGJacColumn(Vector p, Vector pi, Vector zi, Vector c);
   void compute_cog(void);
 
+  void
+  compute_local_interface_forces(double *xdd, double *ad, double *add, double *g,
+				 SL_link li, double *f, double *t);
+
+
 
   // external variables
   extern char config_files[][100];
